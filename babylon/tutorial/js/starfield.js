e513eda3f4fa3c749b475af73af4b6ddf4d8e3d8
David <studerd@gmail.com>
2015-05-19 22:53:25 -0500

    Babylon Starfield
    Add function to limit Camera Zoom
    Generate lots of stars

--- babylon/tutorial/js/starfield.js
+++ babylon/tutorial/js/starfield.js
@@ -2,6 +2,22 @@
 // data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=
 
 
+var camerasBorderFunction = function () {
+        //Angle
+        //if (camera.beta < 0.1)
+            //camera.beta = 0.1;
+        //else if (camera.beta > (Math.PI / 2) * 0.9)
+            //camera.beta = (Math.PI / 2) * 0.9;
+
+  //Zoom
+        if (camera.radius > 2000)
+            camera.radius = 2000;
+
+        if (camera.radius < 10)
+            camera.radius = 10;
+    };
+
+
 function createScene() {
    var scene = new BABYLON.Scene(engine);
    scene.clearColor = new BABYLON.Color3(0, 0, 0);
@@ -28,10 +44,13 @@ function createScene() {
    );
 
 
-   // Add Star to Test
-   var star = new BABYLON.Sprite("star", spriteManagerStar);
-   star.position = new BABYLON.Vector3(0, 0, 0);
-   star.size = 10;
+   // Add Stars Randomly
+   for (var i = 0; i < 3000; i++) {
+      star = new BABYLON.Sprite("star", spriteManagerStar);
+      star.position.x = Math.random() * 2000 - 1000;
+      star.position.y = Math.random() * 2000 - 1000;
+      star.position.z = Math.random() * 2000 - 1000;
+   }
 
 
 
